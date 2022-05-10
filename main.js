f4dba16b3202487a0a245a67f8a22c19b5c8bbcc
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-10 17:07:52 -0500

    Add code lines to style.css and main.js files to dropdown a sidebar occupying the Mobil Version width

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-const mobile = document.querySelector('menu__icon');
-mobile.addEventListener('click', function () {
-  document.addEventListener('slide_menu').classList.toggle('active');
+const open = document.querySelector('.menu__icon');
+open.addEventListener('click', () => {
+  document.getElementById('slide_menu').classList.toggle('active');
 });
+
+const close = document.querySelector('.items');
+close.addEventListener('click', () => {
+  document.getElementById('slide_menu').classList.toggle('inactive');
+});
+
+const close2 = document.querySelector('.cls');
+close2.addEventListener('click', () => {
+  document.getElementById('slide_menu').classList.toggle('inactive');
+});
+
