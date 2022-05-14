d0e04c5370228a4dd905da7637b7b06063f77f5c
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-14 09:42:35 +0000

    Add object superheroes to main.js file

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ close.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.remove('active');
 });
 
+const superheroes = [
+  {
+    name: 'SuperHero #0',
+    description: 'Spiderman: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    image: ['./images/big-image.png'],
+    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
+    linkToLiveVersion: "#",
+    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
+  },
+]
