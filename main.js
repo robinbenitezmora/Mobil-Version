ae5bec8841528364207093a5d7f7ae16487aa441
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-14 05:02:58 -0500

    Complete JavaScript code in main.js file and its object

--- main.js
+++ main.js
@@ -17,4 +17,80 @@ const superheroes = [
     linkToLiveVersion: "#",
     linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
   },
-]
+  {
+    name: 'SuperHero #1',
+    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    image: ['./images/big-image.png'],
+    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
+    linkToLiveVersion: "#",
+    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
+  },
+  {
+    name: 'SuperHero #2',
+    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    image: ['./images/big-image.png'],
+    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
+    linkToLiveVersion: "#",
+    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
+  },
+  {
+    name: 'SuperHero #3',
+    description: 'Spiderman: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    image: ['./images/big-image.png'],
+    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
+    linkToLiveVersion: "#",
+    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
+  },
+  {
+    name: 'SuperHero #4',
+    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    image: ['./images/big-image.png'],
+    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
+    linkToLiveVersion: "#",
+    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
+  },
+  {
+    name: 'SuperHero #5',
+    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
+    image: ['./images/big-image.png'],
+    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
+    linkToLiveVersion: "#",
+    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
+  },
+];
+
+const desktop = document.querySelector('#desktop-modal');
+
+function openPopup(num = null) {
+  if (num != null) {
+    const { technologies } = superheroes[num];
+    let technologiesList = "";
+    technologies.forEach((tech) => {
+      technologiesList += `<li>${tech}</li>`;
+    });
+
+    const { images } = superheroes[num];
+    let imagesList = "";
+    images.forEach((img) => {
+      imagesList += `<img src="${img}"><img>`;
+    });
+
+    document.getElementById('desktop-project').innerHTML = superheroes[num].name;
+    document.getElementById('desktop-image').src = superheroes[num].image;
+    document.getElementById('desktop-open-live').innerHTML = superheroes[num].linkToLiveVersion;
+    document.getElementById('desktop-source').innerHTML = superheroes[num].linkToSource;
+    document.getElementById('desktop-technology').innerHTML = technologiesList;
+    document.getElementById('').innerHTML = imagesList;
+
+    desktop.classList.add('active');
+  }
+}
+
+function closepopup(id = null) {
+  if (id != null) {
+    desktop.classList.remove('active');
+  }
+}
+
+openPopup();
+closepopup();
