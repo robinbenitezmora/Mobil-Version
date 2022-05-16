4e28c04af97313bb5cfdb72d12ab0fb52db1e624
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-16 16:04:22 -0500

    Add main.js file to dinamize version portfolio

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const open = document.querySelector('.menu__icon');
-open.addEventListener('click', () => {
-  document.getElementById('slide_menu').classList.add('active');
-});
 
-const close = document.querySelector('#slide_menu');
-close.addEventListener('click', () => {
-  document.getElementById('slide_menu').classList.remove('active');
-});
