be8cf36ca71c5e6d4894be3b10af4d4a5a71993f
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-17 10:10:50 -0500

    Add code lines in main.js file in order to create the sidebar

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const open = document.querySelector('.menu__icon');
+open.addEventListener('click', () => {
+  document.getElementById('slide_menu').classList.toggle('active');
+});
+
+const close = document.querySelector('.cls');
+close.addEventListener('click', () => {
+  document.getElementById('slide_menu').classList.remove('active');
+});
 
