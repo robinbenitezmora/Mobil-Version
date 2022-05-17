a1d460c13bbc4cb8c8136f1d6446fec4e9ec14fe
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-17 11:02:21 -0500

    Correct linters errors on main.js file version 2

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ open.addEventListener('click', () => {
 });
 
 const close = document.querySelector('.cls');
-close.addEventListener('click', () => {  
+open.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.remove('active');
 });
