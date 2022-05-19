049667468f365778c90d43a9eec8438877d55b47
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-18 23:31:48 -0500

    Add code lines in main.js in order to show popup card

--- main.js
+++ main.js
@@ -1,106 +1,86 @@
+// Creating constants
+
+const openModal = document.getElementById('openDesktop');
+const closeModal = document.getElementById('closeDesktop');
 const open = document.querySelector('.menu__icon');
+const close = document.querySelector('.cls');
+
+// Menu Project
+
 open.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.toggle('active');
 });
 
-const close = document.querySelector('.cls');
 close.addEventListener('click', () => {
   document.getElementById('slide_menu').classList.remove('active');
 });
 
-// Creating constants
-
-const menu = document.querySelector('.menu__icon');
-const nav = document.querySelector('.nav__menu');
-const body = document.querySelector('body');
-const desktop = document.getElementById('desktop-modal');
-const header = document.querySelector('header');
-const openModal = document.getElementById('openDesktop');
-const closeModal = document.getElementById('closeDesktop');
-
-// Popup Window Project
+// Pop Window Project
 
 openModal.addEventListener('click', () => {
-  desktop.classList.add('active');
+  document.getElementById('desktop-modal').classList.add('active');
 });
 
 closeModal.addEventListener('click', () => {
-  desktop.classList.remove('active');
-})
+  document.getElementById('desktop-modal').classList.remove('active');
+});
 
-const superheroes = [
+const projects = [
   {
-    name: 'Spiderman',
-    description: 'Spiderman: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    image: ['./images/big-image.png'],
-    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
-    linkToLiveVersion: '#',
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: ['./images/tonic.svg'],
+    technologies: ['html', 'css', 'JavaScript'],
+    linkToLiveVersion: 'https://www.nasa.gov/',
     linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
   },
   {
-    name: 'Iron Man',
-    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    image: ['./images/big-image.png'],
-    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
-    linkToLiveVersion: '#',
+    name: 'Multi-Post Stories',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: ['./images/big-image4.png'],
+    technologies: ['html', 'Ruby on rails', 'css', 'JavaScript'],
+    linkToLiveVersion: 'https://www.nasa.gov/',
     linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
   },
   {
-    name: 'Hulk',
-    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    image: ['./images/big-image.png'],
-    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
-    linkToLiveVersion: '#',
+    name: 'Facebook 360',
+    description: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    image: ['./images/photo-portfolio.svg'],
+    technologies: ['html', 'Ruby on Rails', 'css', 'JavaScript'],
+    linkToLiveVersion: 'https://www.nasa.gov/',
     linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
   },
   {
-    name: 'American Chaptain',
-    description: 'Spiderman: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    image: ['./images/big-image.png'],
-    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
-    linkToLiveVersion: '#',
-    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
-  },
-  {
-    name: 'Superman',
-    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    image: ['./images/big-image.png'],
-    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
-    linkToLiveVersion: '#',
-    linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
-  },
-  {
-    name: 'Wonderwoman',
-    description: 'Spiderman:  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum molestias, quod recusandae asperiores similique quos aperiam aspernatur, ipsam autem fuga minima libero dolor accusantium corporis possimus quaerat repellat harum distinctio!',
-    image: ['./images/big-image.png'],
-    technologies: ['HTML/CSS', 'JavaScript', 'Bootstrap', 'Ruby on Rails'],
-    linkToLiveVersion: '#',
+    name: 'Uber Navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    image: ['./images/big-image2.png'],
+    technologies: ['html', 'Ruby on Rails', 'css', 'JavaScript'],
+    linkToLiveVersion: 'https://www.nasa.gov/',
     linkToSource: 'https://github.com/robinbenitezmora/Mobil-Version',
   },
 ];
 
-
-function openProjects (num = null) {
-  if (num != null) {
-    const { technologies } = superheroes[num];
+function openProjects(num) {
+  if (num < projects.length) {
     let technologiesList = '';
-    technologies.forEach((tech) => {
+    projects[num].technologies.forEach((tech) => {
       technologiesList += `<li>${tech}</li>`;
     });
 
-    document.getElementById('desktop-project').innerHTML = superheroes[num].name;
-    document.getElementById('desktop-main-image').src = superheroes[num].image;
-    document.getElementById('desktop-open-live').innerHTML = superheroes[num].linkToLiveVersion;
-    document.getElementById('desktop-open-source').innerHTML = superheroes[num].linkToSource;
+    document.getElementById('desktop-project').innerHTML = projects[num].name;
+    document.getElementById('desktop-description').innerHTML = projects[num].description;
+    document.getElementById('desktop-main-image').src = projects[num].image;
+    document.getElementById('desktop-open-live').innerHTML = projects[num].linkToLiveVersion;
+    document.getElementById('desktop-open-source').src = projects[num].linkToSource;
     document.getElementById('desktop-technology').innerHTML = technologiesList;
 
-    desktop.classList.add('active');
+    document.getElementById('desktop-modal').classList.add('active');
   }
 }
 
-function closeProjects (id = null) {
+function closeProjects(id = null) {
   if (id != null) {
-    desktop.classList.remove('active');
+    document.getElementById('desktop-modal').classList.remove('active');
   }
 }
 
