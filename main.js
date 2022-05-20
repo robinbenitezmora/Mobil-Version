2815fe125a6fef3a7d85f122f15d40ee51034c1f
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-19 22:02:33 -0500

    Add code in index.html and main.js in order to create the dynamic popup windows in second and thid sheet

--- main.js
+++ main.js
@@ -25,6 +25,155 @@ closeModal.addEventListener('click', () => {
   document.getElementById('desktop-modal').classList.remove('active');
 });
 
+// Code following the video for second-sheet and third-sheet in index.html
+
+const second_sheet = document.querySelector('.second-sheet');
+const section2 = document.createElement('section2');
+second_sheet.appendChild(section2);
+section.innerHTML = `<section class="second-sheet" id="portfolio">
+<section class="second-sheet__area">
+  <img class="second-sheet__area-big-image" src="./images/tonic.svg" alt="tonic">
+  <article>
+    <h2 class="second-sheet__area-title">Tonic</h2>
+    <div class="second-sheet__area-frame">
+      <h4 class="second-sheet__area-frame-canopy">CANOPY</h4>
+      <img class="second-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+      <h4 class="second-sheet__area-frame-BackEndDev">Back End Dev</h4>
+      <img class="second-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+      <h5 class="second-sheet__area-frame-BackEndDev">2015</h5>
+    </div>
+    <p class="second-sheet__paragraph">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+    <div class="footer">
+      <div class="footer__languages">
+        <div class="footer__languages-html footer__languages-text">html</div>
+        <div class="footer__languages-css footer__languages-text">css</div>
+        <div class="footer__languages-javascript footer__languages-text">javaScript</div>
+      </div>
+      <a class="footer__button footer__button-text" id="openDesktop" href="#" class="Project2">See Project</a>
+    </div>
+  </article>
+</section>
+</section>
+`;
+
+const openProject2 = document.querySelector('.Project2');
+openProject1.addEventListener('click', () => {
+  const main2 = document.createElement('div');
+  const popup2 = document.createElement('div');
+  popup2.innerHTML = `
+  <section class="second-sheet" id="portfolio">
+<section class="second-sheet__area">
+  <img class="second-sheet__area-big-image" src="./images/tonic.svg" alt="tonic">
+  <article>
+    <h2 class="second-sheet__area-title">Tonic</h2>
+    <div class="second-sheet__area-frame">
+      <h4 class="second-sheet__area-frame-canopy">CANOPY</h4>
+      <img class="second-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+      <h4 class="second-sheet__area-frame-BackEndDev">Back End Dev</h4>
+      <img class="second-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+      <h5 class="second-sheet__area-frame-BackEndDev">2015</h5>
+    </div>
+    <p class="second-sheet__paragraph">A daily selection of privately personalized reads; no accounts or sign-ups required.</p>
+    <div class="footer">
+      <div class="footer__languages">
+        <div class="footer__languages-html footer__languages-text">html</div>
+        <div class="footer__languages-css footer__languages-text">css</div>
+        <div class="footer__languages-javascript footer__languages-text">javaScript</div>
+      </div>
+      <a class="footer__button footer__button-text" id="openDesktop" href="#" class="closeProject">See Project</a>
+    </div>
+  </article>
+</section>
+</section>
+`;
+main2.appendChild(popup2);
+second_sheet.appendChild(main2);
+
+const general_close = document.querySelector('.closeProject');
+general_close.addEventListener('click', () => {
+  second_sheet.removeChild(main2);
+})
+});
+
+const third_sheet = document.querySelector('.third-sheet');
+const section3 = document.createElement('section3');
+second_sheet.appendChild(section3);
+section3.innerHTML = `  <section class="third-sheet">
+<section class="third-sheet__area">
+  <article>
+    <h2 class="third-sheet__area-title">Multi-Post Stories</h2>
+    <div class="third-sheet__area-frame">
+      <h4 class="third-sheet__area-frame-canopy">FACEBOOK</h4>
+      <img class="third-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+      <h4 class="third-sheet__area-frame-BackEndDev">Full Stack Dev</h4>
+      <img class="third-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+      <h5 class="third-sheet__area-frame-BackEndDev">2015</h5>
+    </div>
+    <p class="third-sheet__paragraph">Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.</p>
+    <div class="footer">
+      <div class="footer__languages">
+        <div class="footer__languages-html footer__languages-text">html</div>
+        <div class="footer__languages-css footer__languages-text">Ruby on rails</div>
+        <div class="footer__languages-css footer__languages-text">css</div>
+        <div class="footer__languages-javascript footer__languages-text">javaScript</div>
+      </div>
+      <a class="footer__button footer__button-text" href="" class="Project3">See Project</a>
+    </div>
+  </article>
+  <img class="third-sheet__area-big-image" src="./images/big-image4.png" alt="personal-image">
+</section>
+</section>
+`;
+
+const openProject3 = document.querySelector('.Project3');
+openProject1.addEventListener('click', () => {
+  const main3 = document.createElement('div');
+  const popup3 = document.createElement('div');
+  popup3.innerHTML = `
+  <section class="third-sheet">
+    <section class="third-sheet__area">
+      <article>
+        <h2 class="third-sheet__area-title">Multi-Post Stories</h2>
+        <div class="third-sheet__area-frame">
+          <h4 class="third-sheet__area-frame-canopy">FACEBOOK</h4>
+          <img class="third-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+          <h4 class="third-sheet__area-frame-BackEndDev">Full Stack Dev</h4>
+          <img class="third-sheet__area-frame-dot" src="./images/dot.png" alt="dot">
+          <h5 class="third-sheet__area-frame-BackEndDev">2015</h5>
+        </div>
+        <p class="third-sheet__paragraph">Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.</p>
+        <div class="footer">
+          <div class="footer__languages">
+            <div class="footer__languages-html footer__languages-text">html</div>
+            <div class="footer__languages-css footer__languages-text">Ruby on rails</div>
+            <div class="footer__languages-css footer__languages-text">css</div>
+            <div class="footer__languages-javascript footer__languages-text">javaScript</div>
+          </div>
+          <a class="footer__button footer__button-text" href="#" class="closeProject">See Project</a>
+        </div>
+      </article>
+      <img class="third-sheet__area-big-image" src="./images/big-image4.png" alt="personal-image">
+    </section>
+  </section>
+`;
+main3.appendChild(popup3);
+third_sheet.appendChild(main3);
+
+const general_close = document.querySelector('.closeProject');
+general_close.addEventListener('click', () => {
+  third_sheet.removeChild(main3);
+})
+});
+
+
+
+
+
+
+
+
+
+/*
 const projects = [
   {
     name: 'Tonic',
