421a6ff880e0b16e894ea2721ca37727a4527c45
Ben Kiboma <omayioben@gmail.com>
2022-05-21 21:47:41 +0300

    Pop up trial

--- main.js
+++ main.js
@@ -73,3 +73,41 @@ const closeDetail = () => {
 
 openDetail();
 closeDetail();
+
+
+/*Window pop up*/
+const openModalB=document.querySelectorAll('.footer__button');
+
+openModalB.forEach(button => {
+  button.addEventListener('click', () => {
+  const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+    
+})
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+  const modal = button.closest('.modal')
+    closeModal(modal)
+})
+})
+
+function openModal(modal) {
+  if (modal==null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal==null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
