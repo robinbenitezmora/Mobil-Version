7d7f48276de44f3068d1b1c5190613e14fc5be4f
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-21 17:27:51 -0500

    Add code validating data in forms

--- main.js
+++ main.js
@@ -15,33 +15,42 @@ var messageError = document.getElementById('message-error');
 var submitError = document.getElementById('submit-error');
 
 function validateName () {
-  let name = document.getElementById('contact-name').value;
+  const name = document.getElementById('contact-name').value;
 
   if (name.length == 0) {
     nameError.innerHTML = 'Your name is required';
     return false;
   }
-  if (name.match(/[A-Za-z]*\s{1}[A-Za-z]*&/)) {
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*&/)) {
     nameError.innerHTML = 'Please write your name';
     return false;
   }
-  nameError.innerHTML = 'valid';
+  nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
 }
 function validateName ();
 function validateEmail () {
-  let email = document.getElementById('contact-email').value;
+  const email = document.getElementById('contact-email').value;
   
-  if (email.match(^[A-Z]+$)) {
-    emailError.innerHTML = 'Email does not have lower Cases';
+  if (email.length === 0) {
+    emailError.innerHTML = 'Email is required';
     return false;
   }
+
+  if (!email.match(/^[a-z._\-[0-9]*[@][a-z]*\.[a-z]{2,4}$/)) {
+    emailError.innerHTML = 'Email invalid';
+    return false;
+  }
+  emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
   return true;
 }
 
 function validateForm() {
-  if (!validateName() || !validateEmail() || validateMessage()) {
+  if (!validateName() || !validateEmail()) {
+    submitError.style.display = 'block';
     submitError.innerHTML = 'Please check a error before submit';
-    
+    setTimeout(function(){ submitError.style.display = 'none'; }, 2500);
+    return false;
   }
+  return true;
 }
